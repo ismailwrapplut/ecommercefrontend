3af67509d23914f58707a4c7289592c9c901b75f
Mohd Ismail <smileweb.developer@gmail.com>
2023-07-22 12:24:38 +0530

    fixed success message

--- src/components/SuccessMsg/SuccessMsg.js
+++ src/components/SuccessMsg/SuccessMsg.js
@@ -1,11 +1,14 @@
 import Swal from "sweetalert2";
-
+import { resetSuccessAction } from "../../redux/slices/globalActions/globalActions";
 const SuccessMsg = ({ message }) => {
+  const dispatch = useDispatch();
+
   Swal.fire({
     icon: "success",
     title: "Good job!",
     text: message,
   });
+  dispatch(resetSuccessAction());
 };
 
 export default SuccessMsg;
