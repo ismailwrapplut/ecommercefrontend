3f61f7c8e38da9542a0f1b0eabd60de476916f35
Mohd Ismail <smileweb.developer@gmail.com>
2023-07-22 12:27:42 +0530

    fixed success messagde

--- src/components/SuccessMsg/SuccessMsg.js
+++ src/components/SuccessMsg/SuccessMsg.js
@@ -1,5 +1,6 @@
 import Swal from "sweetalert2";
 import { resetSuccessAction } from "../../redux/slices/globalActions/globalActions";
+import { useDispatch } from "react-redux";
 const SuccessMsg = ({ message }) => {
   const dispatch = useDispatch();
 
